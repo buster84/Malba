8190a25f91d6dc200e94dcd43b0775a4c006bfd0
buster84 <okumura.y@shanon.co.jp>
2014-07-30 15:46:18 +0400

    Fix: Publish setting

--- project/Publish.scala
+++ project/Publish.scala
@@ -9,13 +9,13 @@ object Publish {
   lazy val publishLocalInMaven = TaskKey[Unit]("publish-local", "publish local for m2")
 
   lazy val settings = Credential.settings ++ Seq(
-    otherResolvers := Seq(Resolver.file("dotM2", file(Path.userHome + "/.m2/repository")), Resolver.url("Shanon Maven Repo", new java.net.URL(Credential.repo + "/libs-snapshot-local"))),
+    otherResolvers := Seq(Resolver.file("dotM2", file(Path.userHome + "/.m2/repository")), Resolver.url("Shanon Maven Repo", new java.net.URL(Credential.repo + "/libs-release-local"))),
     publishLocalConfiguration in MavenCompile <<= (packagedArtifacts, deliverLocal, ivyLoggingLevel) map {
       (arts, _, level) => new PublishConfiguration(None, "dotM2", arts, Seq(), level)
     },
     publishMavenStyle in MavenCompile := true,
     publishLocal in MavenCompile <<= publishTask(publishLocalConfiguration in MavenCompile, deliverLocal),
     publishLocalInMaven <<= Seq(publishLocal in MavenCompile).dependOn,
-    publishTo := Some(Resolver.url("Shanon Maven Repo", new java.net.URL(Credential.repo + "/libs-snapshot-local")))
+    publishTo := Some(Resolver.url("Shanon Maven Repo", new java.net.URL(Credential.repo + "/libs-release-local")))
   )
 }

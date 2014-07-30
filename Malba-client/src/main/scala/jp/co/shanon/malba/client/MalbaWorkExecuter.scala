204ab139f6c9c9722351ae0847d2c225b09c1e19
buster84 <okumura.y@shanon.co.jp>
2014-07-30 11:14:13 +0400

    Add awaitTermination command

--- Malba-client/src/main/scala/jp/co/shanon/malba/client/MalbaWorkExecuter.scala
+++ Malba-client/src/main/scala/jp/co/shanon/malba/client/MalbaWorkExecuter.scala
@@ -1,7 +1,6 @@
 package jp.co.shanon.malba.client
 
 import java.util.UUID
-import scala.concurrent.duration._
 import akka.actor.Actor
 import akka.actor.ActorLogging
 import akka.actor.ActorRef
@@ -14,7 +13,11 @@ import akka.actor.SupervisorStrategy.Restart
 import akka.actor.ActorInitializationException
 import akka.actor.DeathPactException
 import akka.pattern.pipe
+import akka.pattern.ask
+import akka.util.Timeout
 import org.joda.time.DateTime
+import scala.concurrent._
+import scala.concurrent.duration._
 import jp.co.shanon.malba.worker.WorkerProtocol.TaskIsReady
 import jp.co.shanon.malba.worker.MalbaProtocol
 import jp.co.shanon.malba.worker.Task
@@ -30,6 +33,32 @@ object MalbaWorkManager {
     maxRetryCount: Int ): Props = {
     Props(classOf[MalbaWorkManager], malbaClient, taskType, workExecutorProps, pollingInterval, workTimeout, maxRetryCount)
   }
+
+  def setStopping(workManager: ActorRef): Unit = {
+    workManager ! MalbaWorkProtocol.Closing
+  }
+
+  def awaitTermination(workManager: ActorRef): Unit = {
+    setStopping(workManager)
+    while(isWorking(workManager)){
+      setStopping(workManager)
+      Thread.sleep(30000)
+    }
+  }
+
+  def awaitTermination(workManager: ActorRef, timeout: FiniteDuration): Unit = {
+    setStopping(workManager)
+    val deadline = Deadline.now + timeout
+    while(isWorking(workManager) && !deadline.isOverdue){
+      setStopping(workManager)
+      Thread.sleep(30000)
+    }
+  }
+
+  def isWorking(workManager: ActorRef): Boolean = {
+    implicit val timeout = Timeout(Duration(60, SECONDS))
+    Await.result((workManager ask MalbaWorkProtocol.IsWorking).mapTo[Boolean], Duration(60, SECONDS))
+  }
 }
 
 class MalbaWorkManager(
@@ -42,7 +71,8 @@ class MalbaWorkManager(
 ) extends Actor with ActorLogging {
   import context.dispatcher
   override def supervisorStrategy = OneForOneStrategy() {
-    case _: Exception => 
+    case e: Exception => 
+      log.error(s"Unknow error happen. message: ${e.getMessage} ${e.getStackTrace}")
       receiverWithConnecting(idle)
       Stop
   }
@@ -52,6 +82,7 @@ class MalbaWorkManager(
   val taskPolling    = context.system.scheduler.schedule(pollingInterval, pollingInterval, self, TaskIsReady)
   val connectingTick = context.system.scheduler.schedule(Duration(0, SECONDS), Duration(30, SECONDS), self, Connect)
 
+  var isStopping: Boolean = false
 
   override def postStop(): Unit = {
     taskPolling.cancel()
@@ -90,12 +121,16 @@ class MalbaWorkManager(
   }
 
   def idle: Receive = {
+    case MalbaWorkProtocol.IsWorking => sender() ! false
     case TaskIsReady => 
-      malbaClient.getTask(taskType) pipeTo self
-      receiverWithConnecting(waitForTask)
+      if(!isStopping){
+        malbaClient.getTask(taskType) pipeTo self
+        receiverWithConnecting(waitForTask)
+      }
   }
 
   def waitForTask: Receive = {
+    case MalbaWorkProtocol.IsWorking => sender() ! true
     case MalbaProtocol.GetTaskResponse( id, from, taskType, status, task ) =>
       val worker = context.watch(context.actorOf(workExecutorProps))
       context.setReceiveTimeout(workTimeout)
@@ -112,27 +147,48 @@ class MalbaWorkManager(
   }
 
   def working(worker: ActorRef, task: Task, startDate: DateTime = DateTime.now(), tryCount: Int = 1): Receive = {
+    case MalbaWorkProtocol.IsWorking => sender() ! true
+
     case MalbaWorkProtocol.WorkComplete =>
       context.setReceiveTimeout(Duration.Undefined)
       val duration = new org.joda.time.Duration(startDate, DateTime.now())
       log.info(s"Finished task taskType=${taskType} id=${task.id} complete time=${duration.getStandardSeconds().toString}")
       receiverWithConnecting(idle)
+
     case ReceiveTimeout if maxRetryCount > tryCount =>
       log.warning(s"Receive timeout. Try count is ${tryCount.toString} taskType=${taskType}. Trying again.")
       context.unwatch(worker)
       context.stop(worker)
       val newWorker = context.watch(context.actorOf(workExecutorProps))
       receiverWithConnecting(working(newWorker, task, startDate, tryCount + 1))
+      newWorker ! task
+
     case ReceiveTimeout =>
       log.error(s"Receive timeout. Try count is ${tryCount.toString} taskType=${taskType}")
       context.unwatch(worker)
       context.stop(worker)
+      context.setReceiveTimeout(Duration.Undefined)
       receiverWithConnecting(idle)
+
+    case Terminated(`worker`) => 
+      log.error(s"Terminated worker[${worker.toString}]. Try count is ${tryCount.toString} taskType=${taskType}.")
+      context.unwatch(worker)
+
+      if(maxRetryCount > tryCount) {
+        log.info(s"Try again taskType=${taskType}")
+        val newWorker = context.watch(context.actorOf(workExecutorProps))
+        receiverWithConnecting(working(newWorker, task, startDate, tryCount + 1))
+        newWorker ! task
+      } else {
+        log.info(s"Become idle taskType=${taskType}")
+        receiverWithConnecting(idle)
+        context.setReceiveTimeout(Duration.Undefined)
+      }
   }
 
   override def unhandled(message: Any): Unit = message match {
-    case TaskIsReady                =>
-    case _                          => super.unhandled(message)
+    case MalbaWorkProtocol.Closing   => isStopping = true
+    case TaskIsReady                 =>
+    case _                           => super.unhandled(message)
   }
-
 }

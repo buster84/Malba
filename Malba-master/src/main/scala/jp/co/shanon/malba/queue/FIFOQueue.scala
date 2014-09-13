7d7eab3fd515daade50ff066cc236bb99ae862a2
okumin <mail@okumin.com>
2014-09-14 01:27:37 +0900

    Use scala.collection.immutable.Queue for FIFOQueue instead of List
    scala.collection.immutable.List is a singly linked list, so it is not efficient(O(n)) to append elements.
    Using scala.collection.immutable.Queue, enqueue will be faster.

--- Malba-master/src/main/scala/jp/co/shanon/malba/queue/FIFOQueue.scala
+++ Malba-master/src/main/scala/jp/co/shanon/malba/queue/FIFOQueue.scala
@@ -1,13 +1,33 @@
 package jp.co.shanon.malba.queue
 import jp.co.shanon.malba.worker.Task
+import scala.collection.immutable.Queue
 
+/**
+ * First In, First Out Queue.
+ * FIFOQueue provides following efficient operations.
+ * - contains
+ * - enqueue
+ * - dequeue
+ * But other operations(deleteById, deleteByGroup) may require much time.
+ */
 class FIFOQueue( config: Map[String, String] = Map.empty[String, String] ) extends CustomQueue( config ) {
-  private var storage: List[(String, String, Task)] = List.empty[(String, String, Task)]
+  private[this] type TaskId = String
+  private[this] type Group = String
+
+  private var storage: Queue[(Group, Task)] = Queue.empty
+  // TaskId -> count of Task
+  private[this] var taskCounter: Map[TaskId, Int] = Map.empty
+  private[this] def updateCounter(id: TaskId, increment: Int): Unit = {
+    val newCount = taskCounter.getOrElse(id, 0) + increment
+    if (newCount == 0) {
+      taskCounter -= id
+    } else {
+      taskCounter += id -> newCount
+    }
+  }
 
   def contains(id: String): Boolean = {
-    storage.exists{
-      case ( taskId, _, _ ) => taskId == id
-    }
+    taskCounter.contains(id)
   }
 
   def isEmpty: Boolean = {
@@ -16,33 +36,47 @@ class FIFOQueue( config: Map[String, String] = Map.empty[String, String] ) exten
 
   def enqueue( task: Task, group: Option[String], option: Map[String, String] ): Unit = {
     val insertingGroup = group.getOrElse("OTHERS")
-    storage = storage ++ Seq((task.id, insertingGroup, task))
+    storage = storage.enqueue((insertingGroup, task))
+    updateCounter(task.id, increment = 1)
   }
 
   // Should call nonEmpty before calling dequeue 
   def dequeue(): Task = {
-    val ( id, group, task ) = storage.head
-    storage = storage.tail
+    val (( _, task ), queue) = storage.dequeue
+    storage = queue
+    updateCounter(task.id, increment = -1)
     task
   }
 
   def deleteById( id: String ): Unit = {
-    storage = storage.filterNot{
-      case ( taskId, _, _ ) => taskId == id
+    if (contains(id)) {
+      storage = storage.filterNot {
+        case ( _, task ) =>
+          if (task.id == id) {
+            updateCounter(id, increment = -1)
+            true
+          } else {
+            false
+          }
+      }
     }
   }
 
   def deleteByGroup( group: String ): Unit = {
     storage = storage.filterNot{
-      case ( _, g, _ ) => g == group
+      case ( g, task ) =>
+        if (g == group) {
+          updateCounter(task.id, increment = -1)
+          true
+        } else {
+          false
+        }
     }
   }
 
-  def getStorage = storage
-
   override def equals( obj: Any ): Boolean = {
     obj match {
-      case o: FIFOQueue => o.getStorage == storage
+      case o: FIFOQueue => o.storage == storage
       case _ => false
     }
   }
